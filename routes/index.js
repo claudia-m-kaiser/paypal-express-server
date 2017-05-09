1ab3d65cdb03b5ecb0b65cad93c6a85b03b5933a
Claudia <claudia.m.kaiser@gmail.com>
2017-05-09 14:45:26 +1000

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var braintree = require('braintree');
+var router = express.Router();
+var gateway = require('../lib/gateway');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+//Get a client token from the braintree gateway
+router.get('/braintree/token', function (req, res) {
+  gateway.clientToken.generate({}, function (err, response) {
+    if(err){
+      console.log("Error while generating the client token. Error message: " + err);
+    }else{
+      res.json(200,{clientToken: response.clientToken});
+    }
+  });
+});
+
+//Use nonce received from web or mobile app to create a transaction
+router.get('/transaction', function (req, res) {
+  var transactionErrors;
+  var amount = req.body.amount; // In production you should not take amounts directly from clients
+  var nonce = req.body.payment_method_nonce;
+
+  gateway.transaction.sale({
+    amount: amount,
+    paymentMethodNonce: nonce,
+    options: {
+      submitForSettlement: true
+    }
+  }, function (err, result) {
+    if (result.success || result.transaction) {
+      res.json({transactionid: result.transaction.id});
+    } else {
+      transactionErrors = result.errors.deepErrors();
+      req.flash('error', {msg: formatErrors(transactionErrors)});
+      res.json({message:'Transaction error'});
+      ;
+    }
+  });
+});
+
+
+module.exports = router;
